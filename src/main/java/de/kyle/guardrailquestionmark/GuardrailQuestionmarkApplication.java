a7ec89aad373bce55cda5e78e999f5e24fc47298
kylekreuter <kyle.kreuter@outlook.de>
2025-11-30 14:53:51 +0100

    Initialize Spring Boot project with Maven setup and basic configurations

---
+++ src/main/java/de/kyle/guardrailquestionmark/GuardrailQuestionmarkApplication.java
@@ -0,0 +1,13 @@
+package de.kyle.guardrailquestionmark;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GuardrailQuestionmarkApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(GuardrailQuestionmarkApplication.class, args);
+  }
+
+}
---
+++ src/test/java/de/kyle/guardrailquestionmark/GuardrailQuestionmarkApplicationTests.java
@@ -0,0 +1,13 @@
+package de.kyle.guardrailquestionmark;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GuardrailQuestionmarkApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
